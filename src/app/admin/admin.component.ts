2ce1f79b8c8b6ee844c8da6d76743cd193140b58
design_jsi <design@jsinformatics.com>
2019-11-20 20:29:42 +0530

    Users api implemented.

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../shared/auth.service';
 import { Router } from '@angular/router';
+import { ApiService } from '../api/api.service';
 
 @Component({
   selector: 'app-admin',
@@ -8,12 +9,21 @@ import { Router } from '@angular/router';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
-
-  constructor(private auth: AuthService, private route: Router) { }
+  private userList:any[] = [];
+  constructor(private auth: AuthService, private route: Router, private api: ApiService) { }
 
   ngOnInit() {
     if (!this.auth.isAuth()) {
       this.route.navigate(['']);
+    } else {
+      this.api.users().then(res =>{
+        console.log('res: ', res);
+        if (res.data != null) {
+          this.userList = res.data;
+        }
+      }, err => {
+        console.log('err: ', err);
+      });
     }
   }
 
--- src/app/api/api.service.ts
+++ src/app/api/api.service.ts
@@ -5,12 +5,18 @@ import { Http, Headers } from '@angular/http';
   providedIn: 'root'
 })
 export class ApiService {
-  private loginUrl: string = "http://jsistudentportal.000webhostapp.com/api/user/login/";
+  private urlLogin: string = "http://jsistudentportal.000webhostapp.com/api/user/login/";
+  private urlGetUsers: string = "http://jsistudentportal.000webhostapp.com/api/user/get/";
   constructor(private http: Http) { }
 
-  public login(email: string, password: string): Promise<any>{
-    return this.http.get(`${this.loginUrl}?email=${email}&password=${password}`)
-    .toPromise()
-    .then(res => res.json()).catch(resion=> resion);
+  public login(email: string, password: string): Promise<any> {
+    return this.http.get(`${this.urlLogin}?email=${email}&password=${password}`)
+      .toPromise()
+      .then(res => res.json()).catch(resion => resion);
+  }
+  public users(): Promise<any> {
+    return this.http.get(`${this.urlGetUsers}`)
+      .toPromise()
+      .then(res => res.json()).catch(resion => resion);
   }
 }
